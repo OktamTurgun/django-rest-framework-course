a32d45394da5242ac18cc314d28b8e5eafe74635
Uktam <uktamturgunov30@gmail.com>
2025-10-23 10:04:47 +0500

    feat: Added CRUD APIViews for Book model and fixed field typo

--- lessons/05-function-crud-views/code/library-project/books/urls.py
+++ lessons/05-function-crud-views/code/library-project/books/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import BookAPIListView
+from .views import BookAPIListView, BookDetailAPIView, BookCreateAPIView, BookUpdateAPIView, BookDeleteAPIView
 
 urlpatterns = [
   path('', BookAPIListView.as_view(), name='book-list'),
+  path('create/', BookCreateAPIView.as_view(), name='book-create'),
+  path('<int:pk>/', BookDetailAPIView.as_view(), name='book-detail'),
+  path('<int:pk>/update/', BookUpdateAPIView.as_view(), name='book-update'),
+  path('<int:pk>/delete/', BookDeleteAPIView.as_view(), name='book-delete'),
 ]
