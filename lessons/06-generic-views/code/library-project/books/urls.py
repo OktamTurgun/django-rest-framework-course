d2cd8ddc2cf4e8f60bfc9d1e86fc327bae6e749d
Uktam <uktamturgunov30@gmail.com>
2025-10-27 09:15:13 +0500

    feat(books): add generic views and fix subtitle field typo

--- lessons/06-generic-views/code/library-project/books/urls.py
+++ lessons/06-generic-views/code/library-project/books/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from .views import BookAPIListView, BookDetailAPIView, BookCreateAPIView, BookUpdateAPIView, BookDeleteAPIView
+from .views import (
+    BookListAPIView, BookDetailAPIView, BookCreateAPIView,
+    BookUpdateAPIView, BookDeleteAPIView, BookListCreateAPIView,
+    BookRetrieveUpdateAPIView, BookRetrieveDestroyAPIView,
+    BookRetrieveUpdateDestroyAPIView
+)
 
 urlpatterns = [
-  path('', BookAPIListView.as_view(), name='book-list'),
-  path('create/', BookCreateAPIView.as_view(), name='book-create'),
-  path('<int:pk>/', BookDetailAPIView.as_view(), name='book-detail'),
-  path('<int:pk>/update/', BookUpdateAPIView.as_view(), name='book-update'),
-  path('<int:pk>/delete/', BookDeleteAPIView.as_view(), name='book-delete'),
+    path('list/', BookListAPIView.as_view(), name='book-list'),
+    path('<int:pk>/', BookDetailAPIView.as_view(), name='book-detail'),
+    path('create/', BookCreateAPIView.as_view(), name='book-create'),
+    path('<int:pk>/update/', BookUpdateAPIView.as_view(), name='book-update'),
+    path('<int:pk>/delete/', BookDeleteAPIView.as_view(), name='book-delete'),
+
+    # Kombinatsiyalangan genericlar
+    path('list-create/', BookListCreateAPIView.as_view(), name='book-list-create'),
+    path('<int:pk>/retrieve-update/', BookRetrieveUpdateAPIView.as_view(), name='book-retrieve-update'),
+    path('<int:pk>/retrieve-destroy/', BookRetrieveDestroyAPIView.as_view(), name='book-retrieve-destroy'),
+    path('<int:pk>/full/', BookRetrieveUpdateDestroyAPIView.as_view(), name='book-full'),
 ]
